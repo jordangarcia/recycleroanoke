62d613e371dd949f18908134523785074ca7aec5
Jordan Garcia <jordan@skill-capped.com>
2013-02-02 22:40:19 -0800

    Hooked up symfony to html homepage

---
+++ src/JG/MainBundle/Controller/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JG\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
---
+++ src/JG/MainBundle/MainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JG\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MainBundle extends Bundle
+{
+}
